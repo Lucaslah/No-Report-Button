60e92a24884d7f239e84f85ddb626ef1f2061316
Lucas Petrino <lucaslah@cssudii.tk>
2022-08-24 17:45:36 -0400

    fix: modmenu error

--- src/main/java/me/lucaslah/ncrb/mixin/GameMenuScreenMixin.java
+++ src/main/java/me/lucaslah/ncrb/mixin/GameMenuScreenMixin.java
@@ -1,8 +1,9 @@
 package me.lucaslah.ncrb.mixin;
 
-import me.lucaslah.ncrb.IScreen;
-import net.minecraft.client.gui.Drawable;
-import net.minecraft.client.gui.screen.*;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.OpenToLanScreen;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.resource.language.I18n;
@@ -11,6 +12,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Objects;
 
 @Mixin(GameMenuScreen.class)
 public abstract class GameMenuScreenMixin extends Screen {
@@ -21,29 +25,31 @@ public abstract class GameMenuScreenMixin extends Screen {
 
 	@Inject(method = "initWidgets()V", at = @At(value = "RETURN"))
 	public void initWidgets(CallbackInfo ci) {
+		final List<ClickableWidget> buttons = Screens.getButtons(this);
+
 		ClickableWidget reportButton = null;
 		boolean reportButtonFound = false;
 
-		for (Drawable drawable : ((IScreen) this).getButtons()) {
-			if (!(drawable instanceof ClickableWidget)) continue;
-
-			ClickableWidget button = (ClickableWidget) drawable;
-
-			if (!button.getMessage().getString().equals(I18n.translate("menu.playerReporting"))) continue;
-
-			reportButton = button;
-			reportButtonFound = true;
-			break;
+		for (ClickableWidget button : buttons) {
+			if (button.getMessage().getString().equals(I18n.translate("menu.playerReporting"))) {
+				button.setWidth(-9999);
+				reportButton = button;
+				reportButtonFound = true;
+			}
 		}
 
 		if (reportButtonFound) {
-			reportButton.visible = false;
+			//reportButton.setWidth(-9999);
+			//reportButton.setMessage(Text.of(""));
+			//reportButton.active = false;
 
 			ButtonWidget openToLanButton = this.addDrawableChild(new ButtonWidget(this.width / 2 + 4, this.height / 4 + 96 + -16, 98, 20, Text.translatable("menu.shareToLan"), (button) -> {
+				assert this.client != null;
 				this.client.setScreen(new OpenToLanScreen(this));
 			}));
 
-			openToLanButton.active = this.client.isIntegratedServerRunning() && !this.client.getServer().isRemote();
+			assert this.client != null;
+			openToLanButton.active = this.client.isIntegratedServerRunning() && !Objects.requireNonNull(this.client.getServer()).isRemote();
 		}
 	}
 }
