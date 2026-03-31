a3dae84596e7f449141f51f381f9e91198e1c842
Lucas Petrino <nsx1lucas@gmail.com>
2026-03-31 21:43:15 +1300

    Fix

--- forge/src/main/java/me/lucaslah/nrb/NoReportButtonForge.java
+++ forge/src/main/java/me/lucaslah/nrb/NoReportButtonForge.java
@@ -1,18 +1,15 @@
 package me.lucaslah.nrb;
 
 import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("noreportbutton")
 public class NoReportButtonForge {
 	public NoReportButtonForge() {
-		MinecraftForge.EVENT_BUS.register(this);
+		ScreenEvent.Init.Post.BUS.addListener(NoReportButtonForge::onScreenInit);
 	}
 
-	@SubscribeEvent
-	public void onScreenInit(ScreenEvent.Init.Post event) {
+	private static void onScreenInit(ScreenEvent.Init.Post event) {
 		NoReportButtonClientHooks.updatePauseMenu(event.getScreen(), event.getScreen().getMinecraft(), event.getListenersList());
 	}
 }
