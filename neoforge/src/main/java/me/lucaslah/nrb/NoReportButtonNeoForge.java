35d130c3f9bbe80706d46133c735307cd088f9b9
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2026-06-24 10:55:07 +1200

    Update to Minecraft 26.2 (#21)

--- neoforge/src/main/java/me/lucaslah/nrb/NoReportButtonNeoForge.java
+++ neoforge/src/main/java/me/lucaslah/nrb/NoReportButtonNeoForge.java
@@ -1,16 +1,9 @@
 package me.lucaslah.nrb;
 
-import net.neoforged.neoforge.client.event.ScreenEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.fml.common.Mod;
 
 @Mod("noreportbutton")
 public class NoReportButtonNeoForge {
 	public NoReportButtonNeoForge() {
-		NeoForge.EVENT_BUS.addListener(NoReportButtonNeoForge::onScreenInit);
-	}
-
-	private static void onScreenInit(ScreenEvent.Init.Post event) {
-		NoReportButtonClientHooks.updatePauseMenu(event.getScreen(), event.getScreen().getMinecraft(), event.getListenersList());
 	}
 }
